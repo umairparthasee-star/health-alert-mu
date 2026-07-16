54d78f95a43140497882666679476d5cf3c0ff65
umairparthasee-star <umair.parthasee@spoonconsulting.com>
2026-07-16 11:32:37 +0400

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center">
+      <h1 className="text-4xl font-bold">
+        Health Alert System
+      </h1>
+    </main>
+  );
+}
